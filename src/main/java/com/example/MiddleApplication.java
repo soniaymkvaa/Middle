4ac996a74043934bd173f4fbd43dd2f35072f481
beheni <anastasiia.beheni@ucu.edu.ua>
2022-11-26 15:57:32 +0200

    first try

--- src/main/java/com/example/MiddleApplication.java
+++ src/main/java/com/example/MiddleApplication.java
@@ -1,13 +1,19 @@
-package com.example.middle;
+package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class MiddleApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MiddleApplication.class, args);
+    }
+    @GetMapping
+    public String getData(){
+        return "Hello World!";
     }
 
 }
