685efe1d19ace27a02b70e52f4f9cb76db04f606
OlesiaOmelchuk <olesyao2003@gmail.com>
2022-12-02 17:41:41 +0200

    added test.http, application.properties and endpoints

--- src/main/java/com/example/MiddleApplication.java
+++ src/main/java/com/example/MiddleApplication.java
@@ -2,14 +2,19 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RequestMapping
 @RestController
+@RequestMapping(path = "api/v1/hello")
 public class MiddleApplication {
     public static void main(String[] args) {
         SpringApplication.run(MiddleApplication.class, args);
+    }
+    @GetMapping
+    public String sayHello() {
+        return "hello";
     }
 }
